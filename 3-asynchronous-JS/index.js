1be8aba4b09d151764b83b75db3bb39c050e5e3d
Taylor-Jacquemin <47189568+Taylor-Jacquemin@users.noreply.github.com>
2025-08-19 15:08:29 -0500

    Section 5 Asynchronous JS: Building promises
    Building promises to resolve the callback hell problem by chaining .then statements to method calls and adding the .catch to deal with errors

--- 3-asynchronous-JS/index.js
+++ 3-asynchronous-JS/index.js
@@ -1,19 +1,44 @@
 //looking into the problem of callback function
 const fs = require('fs');
 const superagent = require('superagent'); 
-
-fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
-    console.log(`Breed: ${data}`);
-
-    superagent
-        .get(`https://dog.ceo/api/breed/${data}/images/random`)
-        .end((err, res) => {
-            if (err) return console.log(err.message);
-             console.log(res.body.message);
-
-        fs.writeFile('dog-img.txt', res.body.message, err => {
-            if (err) return console.log(err.message);
-            console.log('Random dog image saved to file!');
+const readFilePro = file => {
+    return new Promise( (resolve, reject ) => {
+        fs.readFile(file, (err, data) => {
+            if (err) reject('I could not find that file :(')
+            resolve(data);
         });
     });
-});
+};
+
+const writeFilePro = (file, data) => {
+    return new Promise( (resolve, reject) => {
+        fs.writeFile(file, data, err => {
+            if (err) reject('Could not write file :( ')
+            resolve('success');
+        });
+    });
+};
+
+
+readFilePro(`${__dirname}/dog.txt`)
+    .then(data => {
+        console.log(`Breed: ${data}`);
+        return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`)
+    })     //handles fulfilled promises 
+    .then(res => {
+        console.log(res.body.message);
+        return writeFilePro('dog-img.txt', res.body.message);
+    })
+    .then( () => {
+        console.log('Random dog image saved to file!');
+    })
+    .catch(err => {
+        console.log(err);
+    });
+
+
+//using superagent library get to return a promise
+//promise implements concept of future value. 
+//if multiple promises you can chain them instead of using callbacks
+//a resolved promised can be fulfilled or rejected(when there was an error.)
+//chaining with the .then (which fulfills the promises stops call)

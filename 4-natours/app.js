e929893567da6f4970de875596e19ff86436c546
Taylor-Jacquemin <jacquemintg@gmail.com>
2025-11-06 14:31:23 -0600

    Section: Express.- Created a Better File Structure
    1. mounted router for users and tours in app.js
    2. create new folder "routes" containing the files for the routes: users and tours and moved the appropriate code for the routes
    3. created new folder "controllers" containing the files controllers for users and tours and moved the appropriate code containing all CRUD
    4. created server.js file which holds the listen for server call and info for port
    5. changed start call in package.json to call the server.js file
    
    Summary: Start program calls server.js (port listen call) which calls app.js which contains the middleware that calls the route files which calls the controller files which exports the data requested and sends it back.

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -1,6 +1,9 @@
-const fs = require('fs');
 const express = require('express');
 const morgan = require('morgan');
+
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+
 const app = express();
 
 //1.MIDDLEWARES. (a function to modify incoming request data) the data from body is added to the request object
@@ -17,167 +20,8 @@ app.use((req, res, next) => {
     next();
 })
 
-const tours = JSON.parse(
-    fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-);
-
-// 2. ROUTE HANDLERS
-const getAllTours = (req, res)=> {
-    console.log(req.requestTime);
-
-    res.status(200).json({
-        status:'success', 
-        requestedAt: req.requestTime,
-        results: tours.length,
-        data: {
-            tours
-        }
-    });
-};
-
-const getTour = (req, res) => {
-    console.log(req.params);
-    //when multiply string that looks like num by a num it will convert the string to a num
-    const id = req.params.id * 1;  
-    const tour = tours.find(el => el.id === id);
-
-    //if(id > tours.length){
-    if(!tour){
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        });
-    }
-
-    res.status(200).json({
-        status:'success', 
-        data: {
-            tour
-        }
-    });
-};
-
-const createTour = (req, res) => {
-    //console.log(req.body);
+// 3. ROUTES 
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/user', userRouter);
     
-    const newId = tours[tours.length-1].id + 1;
-    const newTour = Object.assign({id: newId}, req.body);
-
-    tours.push(newTour);
-
-    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
-        res.status(201).json({
-            status: 'success',
-            data: {
-                tour: newTour
-            }
-        });
-    });
-};
-
-const updateTour = (req, res) => {
-    
-    if(req.params.id *1  > tours.length){
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        });
-    }
-
-    res.status(200).json({
-        status: 'success',
-        data: {
-            tour: '<Updated tour here...>'
-        }
-    });
-};
-
-const deleteTour = (req, res) => {
-    if(req.params.id *1  > tours.length){
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        });
-    }
-//204 means no content
-    res.status(204).json({
-        status: 'success',
-        data: null
-    });
-};
-
-const getAllUsers = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'This route is not yet defined!'
-    })
-};
-
-const getUser = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'This route is not yet defined!'
-    })
-};
-
-const createUser = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'This route is not yet defined!'
-    })
-};
-
-const updateUser = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'This route is not yet defined!'
-    })
-};
-
-const deleteUser = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'This route is not yet defined!'
-    })
-};
-
-//restructuring part 1<--separated handlers to be exported to another file later
-//app.get('/api/v1/tours', getAllTours);
-//app.get('/api/v1/tours/:id', getTour);
-//app.post('/api/v1/tours', createTour);
-//app.patch('/api/v1/tours/:id', updateTour);
-//app.delete('/api/v1/tours/:id', deleteTour);
-
-//restructuring part 2<--easier to read plus we don't want to have to change 
-//data in all 5 places if url changes
-
-
-// 3. ROUTE
-app
-    .route('/api/v1/tours')
-    .get(getAllTours)
-    .post(createTour);
-
-app 
-    .route('/api/v1/tours/:id')
-    .get(getTour)
-    .patch(updateTour)
-    .delete(deleteTour);
-
-app
-    .route('/api/v1/users')
-    .get(getAllUsers)
-    .post(createUser);
-
-app
-    .route('/api/vq/users/:id')
-    .get(getUser)
-    .patch(updateUser)
-    .delete(deleteUser);
-    
-// 4. SERVER    
-const port = 3000;
-app.listen(port, () => {
-    console.log(`App running on port ${port}...`);
-
-});
+module.exports = app;

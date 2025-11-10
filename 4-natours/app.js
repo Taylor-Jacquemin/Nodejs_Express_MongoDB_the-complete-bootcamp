7c613cbe4c92c301dd185fce407ef71ed0123d8c
Taylor-Jacquemin <jacquemintg@gmail.com>
2025-11-10 16:07:30 -0600

    Express Section: Chaining Multiple Middleware functions
    tourController.js
    - added check for name/ price in body of message
    tourRoutes.js
    -chained the call for the checkbody middle ware

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
     next();
-})
+});
 
 // 3. ROUTES 
 app.use('/api/v1/tours', tourRouter);

d7c9dea56769598ad85e83ff68efccf4ebb6d1a8
Taylor-Jacquemin <jacquemintg@gmail.com>
2025-11-13 09:42:40 -0600

    Express Section-end
    Added ESLint plugins
    preparation for MongoDB section

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -1,7 +1,21 @@
+const dotenv = require('dotenv');
 const app = require('./app');
 
-const port = 3000;
+dotenv.config({ path: './config.env' });
+
+
+//gets environment variable (global variable used for enviornment in which node app is running)
+//console.log(app.get('env'));
+//allows setting of env variables with call NODE_ENV=development nodemon server.js
+//console.log(process.env);
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`App running on port ${port}...`);
-
 });
+//if not related to express its done outside the app file 
+//environment variables
+
+
+
+

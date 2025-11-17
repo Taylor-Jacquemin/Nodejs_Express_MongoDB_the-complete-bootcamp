1026a0eb8c3b69189d83c65655bd2d4e816bbdae
Taylor-Jacquemin <jacquemintg@gmail.com>
2025-11-17 11:06:54 -0600

    Section 8: Using MongoDB with Mongoose
    package.json/package-lock.json
    -updated dependents' versions
    config.env
    -connected database with express app
    server.js
    -created simple tour model
    -created docs and tested the model

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -1,13 +1,54 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const app = require('./app');
 
 dotenv.config({ path: './config.env' });
+const app = require('./app');
 
+const DB = process.env.DATABASE.replace(
+    '<PASSWORD>',
+     process.env.DATABASE_PASSWORD
+);
 
-//gets environment variable (global variable used for enviornment in which node app is running)
-//console.log(app.get('env'));
-//allows setting of env variables with call NODE_ENV=development nodemon server.js
-//console.log(process.env);
+//will return promise
+
+mongoose.connect(DB).then(() => {  
+    console.log('DB connection successful');
+ }).catch((err) => {
+        console.log(err)
+});
+
+const tourSchema = new mongoose.Schema( { 
+    name: {
+        type:String, 
+        required: [true, 'A tour must have a name'],
+        unique: true
+    },
+    rating: {
+        type: Number, 
+        default: 4.5
+
+    },
+    price: {
+        type: Number,
+        required: [true, 'A tour must have a price']
+    }
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+    name: 'The Park Camper',
+    price: 997
+});
+
+testTour
+    .save()
+    .then(doc => {
+        console.log(doc);
+    })
+    .catch(err => {
+        console.log('ERROR: 💥💥', err);
+    });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
